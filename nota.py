d178517c38c134da9f132d2a9f82e65bfa26bad6
zeroylalus <armopc8@gmail.com>
2023-04-21 21:29:22 -0600

    test

---
+++ nota.py
@@ -0,0 +1,45 @@
+"""
+* EJEMPLO
+! EJEMPLO
+? EJEMPLO
+TODO: EJEMPLO
+
+? GOOGLEAR ARCHIVOS:
+asgi/wsgi: por que 'asgy'?
+'import os'
+'from django.core.asgi import get_asgi_application'
+'os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reports_proj.settings')'
+'application = get_asgi_application()'
+
+? tutorial de signals?
+
+? desintalar framework:
+pip uninstall Django
+pip install Django==3.1.7
+
+? comandos en cmd:
+pwd=cd y ls=dir, mv=ren, cd ..
+
+? instalar desde requirements.txt:
+cd desktop
+virtualenv test
+cd test
+.\Scripts\activate
+pip install -r requirements.txt
+
+-----------------------
+
+[BASE_DIR / 'templates'],
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS=[
+    BASE_DIR / 'static',
+    BASE_DIR / 'sales' / 'static'
+]
+
+MEDIA_URL ='/media/'
+
+MEDIA_ROOT=BASE_DIR / 'media'
+
+"""
---
+++ seguimiento.py
@@ -0,0 +1,38 @@
+""" 
+* EJEMPLO
+! EJEMPLO
+? EJEMPLO
+TODO: EJEMPLO
+
+? VIRTUAL ENV:
+virtualenv venv
+.venv\Scripts\activate
+
+? PIP
+py.exe -m pip install --upgrade pip
+pip install django
+pip install django-crispy-forms
+pip install pandas
+pip install matplotlib
+pip install pillow
+pip install seaborn
+pip install xhtml2pdf
+pip install crispy-bootstrap4
+
+pip freeze > requirements.txt
+
+? PROJECT
+django-admin startproject reports_proj .
+py manage.py migrate
+py manage.py createsuperuser
+
+? MANAGE
+py manage.py startapp sales
+py manage.py startapp reports
+py manage.py startapp profiles
+py manage.py startapp products
+py manage.py startapp customers
+
+py manage.py runserver
+
+"""
